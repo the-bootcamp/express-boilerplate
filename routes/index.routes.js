de94ef0c06447b1b212e70ec3eb4112040003c36
Giovanna Izzo <giovannapenelopeizzo@gmail.com>
2020-08-25 18:08:00 +0200

    added routes and created signup.hbs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+/* GET landing page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET signup page */
+router.get('/signup', (req, res, next) => res.render('signup'));
+
+
+
+
+
 
 module.exports = router;
