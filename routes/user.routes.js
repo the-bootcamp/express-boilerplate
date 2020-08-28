8138e8a340a228b42499fa55362790f883c63642
Nuno Pacheco <nunorpacheco@gmail.com>
2020-08-28 16:55:54 +0100

    edit/delete routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,13 +4,39 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const User = require("../models/User.model");
-const Skill = require("../models/Job.model");
+const Skill = require("../models/Skill.model");
 const Job = require("../models/Job.model");
 const mongoose = require("mongoose");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/profile-user", (req, res, next) => res.render("profileuser"));
+//router.get("/profile-user", (req, res, next) => res.render("profileuser"));
+
+//EDIT USER PROFILE
+router.get('/edit', (req, res) => {
+  res.render('edit-details.hbs')
+}
+
+router.post("/edit", (req, res) => {
+  User.model.findByIdAndUpdate( userData._id, {$set: req.body})
+  .then(() => {
+    res.redirect('profile-user')
+  })
+})
+
+
+//DELETE USER PROFILE
+router.get('/:id/delete', (req, res) => {
+  const {id} = req.params;
+  User.model.findByIdAndDelete(id)
+    .then(() => res.redirect('/signout')) //doing logout when delete it
+    .catch((err) => {
+      console.log(`Error while deleting the profile: ${err}`);
+      next();
+    });
+});
+
+
 
 module.exports = router;
